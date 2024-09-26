9d0e134c66536820e6498e8527362ef628512a75
Rrr <romanchekmezov99@gmail.com>
2024-09-26 16:04:44 +0300

    workmanager now caches retrofit data into room db

---
+++ app/src/main/java/com/example/harry_potter_and_retrofit/presentation/ui/pagingfragment/PagingFragment.kt
@@ -0,0 +1,31 @@
+package com.example.harry_potter_and_retrofit.presentation.ui.pagingfragment
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.harry_potter_and_retrofit.R
+
+class PagingFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = PagingFragment()
+    }
+
+    private val viewModel: PagingViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        return inflater.inflate(R.layout.fragment_paging, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/harry_potter_and_retrofit/presentation/ui/pagingfragment/PagingViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.harry_potter_and_retrofit.presentation.ui.pagingfragment
+
+import androidx.lifecycle.ViewModel
+
+class PagingViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
